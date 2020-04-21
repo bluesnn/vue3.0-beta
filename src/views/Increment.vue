d02539ab35ac935ffdff9c26f7329f0c227735d3
bluesnn <mr.bluesnn@gmail.com>
2020-04-21 17:31:44 +0800

    code init

---
+++ src/views/Increment.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <!-- <button @click="increment">
+      count: {{ state.count }}, double: {{ state.double }}
+    </button> -->
+  </div>
+</template>
+
+<script>
+import { reactive, computed, watchEffect } from 'vue'
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 0,
+      double: computed(() => state.count * 2)
+    })
+
+    function increment() {
+      state.count++
+    }
+
+    watchEffect(() => {
+      document.body.innerHTML = `count is ${state.count}`
+    })
+
+    return {
+      state,
+      increment
+    }
+  }
+}
+</script>
